e2207a0d12d7d289db34fdb96a8c19cc30e67051
Andrey A. But <andrey.but@saritasa.com>
2018-05-04 17:14:36 +0600

    Added eslint for project

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,27 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
-    sourceType: 'module'
+    parser: 'babel-eslint'
   },
   env: {
     browser: true,
+    es6: true,
+    jquery: true,
   },
-  extends: 'airbnb-base',
+  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  // 'plugin:vue/essential',
+
+  extends: [
+    'plugin:vue/recommended',
+    'airbnb-base',
+  ],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'vue',
+    'eslint-comments',
+    'compat',
   ],
   // check if imports actually resolve
   settings: {
@@ -39,11 +49,115 @@ module.exports = {
         'e' // for e.returnvalue
       ]
     }],
+    'object-curly-spacing': ['error', 'always'],
+    'space-in-parens': ['error', 'never'],
     // allow optionalDependencies
     'import/no-extraneous-dependencies': ['error', {
       optionalDependencies: ['test/unit/index.js']
     }],
+    'import/newline-after-import': 'error',
+    'import/no-named-as-default-member': 'off',
+    'no-plusplus': 'off',
+    'max-params': ['error', 3],
+    'comma-style': ['error', 'last'],
+    'comma-dangle': ['error', 'always'],
+    'require-jsdoc': ['error', {
+      'require': {
+        'FunctionDeclaration': true,
+        'MethodDefinition': true,
+        'ClassDeclaration': true,
+        'ArrowFunctionExpression': true,
+      }
+    }],
+    'padding-line-between-statements': [
+      'error',
+      {
+        'blankLine': 'always',
+        'prev': ['const', 'let', 'var'],
+        'next': '*'
+      },
+      {
+        'blankLine': 'always',
+        'prev': '*',
+        'next': ['const', 'let', 'var']
+      },
+      {
+        'blankLine': 'any',
+        'prev': ['const', 'let', 'var'],
+        'next': ['const', 'let', 'var']
+      },
+      {
+        'blankLine': 'always',
+        'prev': '*',
+        'next': ['case', 'default']
+      },
+      {
+        'blankLine': 'always',
+        'prev': ['const', 'let', 'var', 'block', 'block-like'],
+        'next': ['block', 'block-like']
+      },
+      {
+        'blankLine': 'always',
+        'prev': 'directive',
+        'next': '*'
+      },
+      {
+        'blankLine': 'any',
+        'prev': 'directive',
+        'next': 'directive'
+      },
+      {
+        'blankLine': 'always',
+        'prev': '*',
+        'next': 'return'
+      }
+    ],
+    'no-bitwise': ['error', { 'int32Hint': true }],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'dot-location': ['error', 'property'],
+    'object-property-newline': 'error',
+    'no-new': 'warn',
+    'max-len': ['error',
+      {
+        'code': 80,
+        'ignoreComments': true,
+        'ignoreTrailingComments': true,
+        'ignoreUrls': true,
+        'ignoreStrings': true,
+        'ignoreTemplateLiterals': true,
+      }
+    ],
+    'no-shadow': 'off',
+    'array-bracket-spacing': ['error',
+      'always',
+      {
+        'singleValue': false,
+      }
+    ],
+    'no-underscore-dangle': 'off',
+    'spaced-comment': ['error', 'always', { 'exceptions': ['-', '+', '=', '*'] }],
+    'key-spacing': ["error", { "align": "value" }],
+    'eslint-comments/disable-enable-pair': 'warn',
+    'eslint-comments/no-duplicate-disable': 'error',
+    'eslint-comments/no-unlimited-disable': 'error',
+    'eslint-comments/no-unused-disable': 'warn',
+    'eslint-comments/no-unused-enable': 'warn',
+
+    // browser compatibility error
+    // Use caniuse and @kangax's compat table for determining coverage
+    'compat/compat': 'error',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // vuejs
+    'vue/max-attributes-per-line': [2,
+      {
+        'singleline': 1,
+        'multiline': {
+          'max': 1,
+          'allowFirstLine': true
+        }
+      }
+    ],
   }
-}
+};
